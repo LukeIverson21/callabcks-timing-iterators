372f60309c4fc8d31a2ef87a0aed5e954cf44c43
Luke Iverson <lukeiverson@Lukes-MacBook-Pro.local>
2020-10-22 09:15:08 -0500

    10 times repeated

--- hello.js
+++ hello.js
@@ -28,3 +28,37 @@ function sayHello(name) {
     return msg;
 }
 console.log(sayHello('SEI 1019'));
+
+function sayHello(name) {
+    let msg = "Hello, " + name + '. How are you?';
+    return msg;
+}
+console.log(sayHello('SEI 1019'));
+
+function sayHello(name) {
+    let msg = "Hello, " + name + '. How are you?';
+    return msg
+}
+console.log(sayHello('SEI 1019'));
+
+function sayHello(name) {
+    let msg = "Hello, " + name + '. How are you?';
+    return msg
+}
+console.log(sayHello('SEI 1019'));
+
+function sayHello(name) {
+    let msg = "Hello, " + name + '. How are you?';
+    return msg
+}
+console.log(sayHello('SEI 1019'));
+
+function sayHello(name) {
+    let msg = "Hello, " + name + '. How are you?';
+    return msg
+}
+console.log(sayHello('SEI 1019'));
+
+
+
+
